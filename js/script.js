2e7212bc36b75ff2c532411d3b00e0c1508edbd6
Luiz Abreu <lcabreu02@gmail.com>
2024-06-18 19:46:09 -0300

    versao apresentacao

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-$('.carousel').carousel()
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  if (index >= totalSlides) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = totalSlides - 1;
+  } else {
+    currentSlide = index;
+  }
+  const offset = -currentSlide * 100;
+  document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+
+setInterval(nextSlide, 10000); // Muda a imagem a cada 10 segundos
+
+// Navegação manual
+document.querySelector('.next').addEventListener('click', nextSlide);
+document.querySelector('.prev').addEventListener('click', prevSlide);
